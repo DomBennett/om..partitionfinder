4f4f6280528b5687ccd2ac71ea509c3b6b9721cc
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-08-20 13:34:40 +0200

    update

--- examples/PartitionFinder.R
+++ examples/PartitionFinder.R
@@ -1,6 +1,8 @@
 library(outsider)
 PartitionFinder <- module_import('PartitionFinder',
                                  repo = 'dombennett/om..partitionfinder')
+example_fetch <- module_import('example_fetch',
+                               repo = 'dombennett/om..partitionfinder')
 # bring PartitionFinder examples to working directory
 example_fetch(type = 'nucleotide')
 # run on the "nucleotide" example folder
--- examples/PartitionFinderMorphology.R
+++ examples/PartitionFinderMorphology.R
@@ -1,6 +1,8 @@
 library(outsider)
 PartitionFinderMorphology <- module_import('PartitionFinderMorphology',
                                  repo = 'dombennett/om..partitionfinder')
+example_fetch <- module_import('example_fetch',
+                               repo = 'dombennett/om..partitionfinder')
 # bring PartitionFinder examples to working directory
 example_fetch(type = 'morphology')
 # run on the "morphology" example folder
